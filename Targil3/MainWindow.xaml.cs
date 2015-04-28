e0158967c3004f53dd52792650cf1d4cd030e2c6
y00s <yosyes@gmail.com>
2015-04-29 00:00:57 +0300

    first commit

---
+++ Targil3/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Targil4
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ExitClick(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+
+        }
+
+   
+
+        private void Book1MouseEnter(object sender, MouseEventArgs e)
+        {
+            //Storyboard sb = new Storyboard();
+            //DoubleAnimation xAnimation = new DoubleAnimation(1, 0, new Duration(TimeSpan.FromSeconds(2)), FillBehavior.HoldEnd);
+            //Storyboard.SetTarget(xAnimation, Book1);
+            //Storyboard.SetTargetProperty(xAnimation, new PropertyPath("RenderTransform.ScaleX"));
+
+            //DoubleAnimation yAnimation = new DoubleAnimation(1, 0, new Duration(TimeSpan.FromSeconds(2)), FillBehavior.HoldEnd);
+            //Storyboard.SetTarget(yAnimation, Book1);
+            //Storyboard.SetTargetProperty(yAnimation, new PropertyPath("RenderTransform.ScaleY"));
+
+            //sb.Children.Add(xAnimation);
+            //sb.Children.Add(yAnimation);
+            //sb.Begin();
+        }
+
+    }
+}
---
+++ Targil3/valueToCartezianConverters.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Targil4
+{
+  
+        public class SinConverter : IValueConverter
+        {
+            
+
+            public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+            {
+                try
+                {
+                    double angle = System.Convert.ToDouble(value);
+                    double angleRad = Math.PI * angle / 180;
+                    double radius = System.Convert.ToDouble(parameter);
+                    return radius * Math.Sin(angleRad);
+                }
+                catch
+                {
+                    return Binding.DoNothing;
+                }
+            }
+
+            public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+            {
+                throw new NotImplementedException();
+            }
+
+         
+        }
+
+        public class CosConverter : IValueConverter
+        {
+
+
+            object IValueConverter.ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+            {
+                throw new NotImplementedException();
+            }
+
+            object IValueConverter.Convert(object value, Type targetType, object parameter, CultureInfo culture)
+            {
+                try
+                {
+                    double angle = System.Convert.ToDouble(value);
+                    double angleRad = Math.PI * angle / 180;
+                    double radius = System.Convert.ToDouble(parameter);
+                    return radius * Math.Cos(angleRad);
+                }
+                catch
+                {
+                    return Binding.DoNothing;
+                }
+
+            }
+        }
+}
+
